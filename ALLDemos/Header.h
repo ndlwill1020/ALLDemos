0a81ee49841ce9f52572ce84dc0279a6672eeadf
ndl_mac <ndl_mac1020@126.com>
2015-08-11 18:29:38 +0800

    demo commit
    all demos

---
+++ ALLDemos/Header.h
@@ -0,0 +1,23 @@
+//
+//  Header.h
+//  ALLDemos
+//
+//  Created by macuser on 15/8/11.
+//  Copyright (c) 2015年 ndl. All rights reserved.
+//
+
+#ifndef ALLDemos_Header_h
+#define ALLDemos_Header_h
+/*
+ UIViewController。用于自定􏰍视图控制器的导航。例如,对于两个界面的􏶗􏶘,我们可以用一个 UIViewController来控制另外两个UIViewController。
+ 􏱗 UINavigationController。导航控制器,􏰊与UITableViewController结合使用,能􏲇构建树形结 构导航模式。
+ 􏱗 UITabBarController。标签栏控制器,用于构建树标签导航模式。
+ 􏱗 UIPageViewController。􏲲现电子书导航􏱢格的控制器。
+ 􏱗 UISplitViewController。可以把屏幕分􏱣成几􏻙的视图控制器,主要为iPad屏幕设计。
+ 􏱗 UIPopoverController。􏲲现􏰷􏰍􏱤􏰸􏱢格视图的控制器,主要为iPad屏幕设计。
+ 
+ 􏱗 presentViewController:animated:completion。􏲲现模􏰮视图。
+ 􏱗 dismissViewControllerAnimated:completion。关􏲳模􏰮视图。
+ */
+
+#endif
---
+++ ALLDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALLDemos
+//
+//  Created by macuser on 15/8/11.
+//  Copyright (c) 2015年 ndl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
